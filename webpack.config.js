1a185ee8819ce457009b99a7b98579ad884a4656
unknown <mjdease@users.noreply.github.com>
2017-07-30 00:42:48 -0300

    Setup dev/prod builds

--- webpack.config.js
+++ webpack.config.js
@@ -6,90 +6,105 @@ const extractStyles = new ExtractTextPlugin({
   filename: '[name].css',
 });
 
-module.exports = {
-  devtool: 'sourcemap',
-  entry: {
-    popup: './src/popup.jsx',
-    background: './src/background.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'extension/dist'),
-    filename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.jsx?$/,
-        loader: 'eslint-loader',
-      },
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        use: extractStyles.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true,
-              },
-            },
-          ],
-        }),
-      },
-      {
-        test: /\.scss$/,
-        use: extractStyles.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true,
-              },
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true,
-              },
-            },
-          ],
-        }),
-      },
-      {
-        test: [
-          /\.bmp$/,
-          /\.gif$/,
-          /\.jpe?g$/,
-          /\.png$/,
-          /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        ],
-        loader: 'file-loader',
-        options: {
-          name: 'assets/[name].[hash:8].[ext]',
+module.exports = function webpackConfig(environment) {
+  const env = environment || {};
+
+  return {
+    devtool: 'source-map',
+    entry: {
+      popup: './src/popup.jsx',
+      background: './src/background.js',
+    },
+    output: {
+      path: path.resolve(__dirname, 'extension/dist'),
+      filename: '[name].js',
+    },
+    stats: {
+      children: false,
+    },
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.jsx?$/,
+          loader: 'eslint-loader',
         },
-      },
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          use: extractStyles.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
+          }),
+        },
+        {
+          test: /\.scss$/,
+          use: extractStyles.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
+          }),
+        },
+        {
+          test: [
+            /\.bmp$/,
+            /\.gif$/,
+            /\.jpe?g$/,
+            /\.png$/,
+            /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          ],
+          loader: 'file-loader',
+          options: {
+            name: 'assets/[name].[hash:8].[ext]',
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      modules: [
+        path.join(__dirname, 'src'),
+        'node_modules',
+      ],
+    },
+    plugins: [
+      new webpack.NoEmitOnErrorsPlugin(),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: `"${env.production ? 'production' : 'development'}"`,
+        },
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        compress: !!env.production,
+        mangle: !!env.production,
+        output: {
+          beautify: !env.production,
+          comments: false,
+        },
+      }),
+      extractStyles,
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    modules: [
-      path.join(__dirname, 'src'),
-      'node_modules',
-    ],
-  },
-  plugins: [
-    new webpack.NoEmitOnErrorsPlugin(),
-    // React spams warning logs if this isn't defined
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"development"',
-      },
-    }),
-    extractStyles,
-  ],
+  };
 };
